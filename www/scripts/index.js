c5ba810421798b76fd1c174fd3443fe557a5417c
Diego Ezequiel Guillén <diegoezequielguillen@civeloo.com>
2015-11-19 22:24:00 -0300

    optimization

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -11,10 +11,8 @@
         // Handle the Cordova pause and resume events
         document.addEventListener('pause', onPause.bind(this), false);
         document.addEventListener('resume', onResume.bind(this), false);
-        llenarSelect();
-        temporizador();
-
-
+        fillSelect();
+        count();
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
@@ -26,100 +24,113 @@
         // TODO: This application has been reactivated. Restore application state here.
     };
 
-    function llenarSelect() {
+    function count() {
+        var time = {
+            hour: 0,
+            minute: 0,
+            second: 0,
+            centisecond: 0
+        }
+        var chr = false;
+        var time_running = undefined;
+        function resetT() {
+            $("#btn-start").text('start');
+            chr = false;
+            time.hour = 0;
+            time.minute = 0;
+            time.second = 0;
+            time.centisecond = 0;
+            update();
+        }
+        function update() {
+            $("#centisecond")[0].selectedIndex = time.centisecond;
+            $("#second")[0].selectedIndex = time.second;
+            $("#minute")[0].selectedIndex = time.minute;
+            $("#hour")[0].selectedIndex = time.hour;          
+        }
+        function chronometer() {
+            // centiseconds
+            time.centisecond++;
+            if (time.centisecond >= 100) {
+                time.centisecond = 0;
+                time.second++;
+            }
+            // seconds
+            if (time.second >= 60) {
+                time.second = 0;
+                time.minute++;
+            }
+            // minutes
+            if (time.minute >= 60) {
+                time.minute = 0;
+                time.hour++;
+            }
+        }
+        function countdown() {
+            // centiseconds
+            if (time.centisecond > 0) time.centisecond--;
+            if ((time.centisecond == 0) & (time.second > 0)) {
+                time.centisecond = 100;
+                if (time.second > 0) time.second--;
+            }
+            // seconds                       
+            if ((time.second == 0) & (time.minute > 0)) {
+                time.second = 59;
+                time.minute--;
+            }
+            // minutes
+            if ((time.minute == 0) & (time.hour > 0)) {
+                time.minute = 59;
+                time.hour--;
+            }
+        }     
+        function stop() {
+            clearInterval(time_running);
+            time_running = undefined;
+            resetT();
+        }
+        function isZero() {
+            return ((time.hour == 0) & (time.minute == 0) & (time.second == 0) & (time.centisecond == 0)) ? true : false;
+        }
+        function run() {
+            (chr) ? chronometer() : countdown();
+            update();
+            if (isZero()) stop();
+        }        
+        function play() {           
+            time.hour = $("#hour")[0].value;
+            time.minute = $("#minute")[0].value;
+            time.second = $("#second")[0].value;
+            time.centisecond = $("#centisecond")[0].value;
+            if (isZero())  chr = true;
+            $("#btn-start").text('stop');
+            time_running = setInterval(run, 10);
+        }
+      
+        $("#btn-start").click( 
+            function(){
+                ($(this).text() == 'start') ? play() : stop(); 
+            }
+        );
+   }
+
+    function fillSelect() {
         var t = [
+         $("#centisecond")[0],
          $("#second")[0],
          $("#minute")[0],
          $("#hour")[0]
         ];
         for (var j = 0; j < t.length ; j++) {
             t[j].clear;
-            (j < t.length - 1) ? t[j].length = 60 : t[j].length = 24;
+            t[j].length = (j == 0) ? 100 :
+                          (j == 3) ? 24 :
+                          60;
             for (var i = 0; i < (t[j].length) ; i++) {
-                t[j][i].text = i;
-                t[j][i].value = i;
+                t[j][i].text = (i < 10) ? "0" + i : i;                
             }
             t[j].selected = true;
         }
-        $("#hour").selectmenu("refresh");
-        $("#minute").selectmenu("refresh");
-        $("#second").selectmenu("refresh");
     }
 
-    function temporizador() {
-        var tiempo = {
-            hora: 0,
-            minuto: 0,
-            segundo: 0
-        };
-        var cron = false;
-        var tiempo_corriendo = null;
-        $("#btn-comenzar").click(function () {
-            if ($(this).text() == 'Comenzar') {
-                $(this).text('Detener');
-                tiempo.hora = $("#hour")[0].value;
-                tiempo.minuto = $("#minute")[0].value;
-                tiempo.segundo = $("#second")[0].value;
-                tiempo_corriendo = setInterval(
-                function () {
-                    if ((tiempo.hora == 0) & (tiempo.minuto == 0) & (tiempo.segundo == 0)) {
-                        cron = true;
-                    }
-                    if (cron) {
-                        //cronometro
-                        // Segundos
-                        tiempo.segundo++;
-                        if (tiempo.segundo >= 60) {
-                            tiempo.segundo = 0;
-                            tiempo.minuto++;
-                        }
-
-                        // Minutos
-                        if (tiempo.minuto >= 60) {
-                            tiempo.minuto = 0;
-                            tiempo.hora++;
-                        }
-                    }
-                    else {
-                        //temporizador
-                        // Segundos
-                        if (tiempo.segundo > 0) tiempo.segundo--;
-                        if ((tiempo.segundo == 0) & (tiempo.minuto > 0)) {
-                            tiempo.segundo = 59;
-                            tiempo.minuto--;
-                        }
-                        // Minutos
-                        if ((tiempo.minuto == 0) & (tiempo.hora > 0)) {
-                            tiempo.minuto = 59;
-                            tiempo.hora--;
-                        }
-                    }
-
-                    $("#hour")[0].selectedIndex = tiempo.hora;
-                    $("#hour").selectmenu("refresh");
-                    $("#minute")[0].selectedIndex = tiempo.minuto;
-                    $("#minute").selectmenu("refresh");
-                    $("#second")[0].selectedIndex = tiempo.segundo;
-                    $("#second").selectmenu("refresh");
-
-                    if ((tiempo.hora == 0) & (tiempo.minuto == 0) & (tiempo.segundo == 0)) {
-                        $("#btn-comenzar").text('Comenzar');
-                        clearInterval(tiempo_corriendo);
-                        return;
-                    }
-
-                }, 1000);
-            } else {
-                $(this).text('Comenzar');
-                clearInterval(tiempo_corriendo);
-                cron = false;
-                $("#hour")[0].selectedIndex = 0;
-                $("#hour").selectmenu("refresh");
-                $("#minute")[0].selectedIndex = 0;
-                $("#minute").selectmenu("refresh");
-                $("#second")[0].selectedIndex = 0;
-                $("#second").selectmenu("refresh");
-            }
-        })
-    }
 })();
