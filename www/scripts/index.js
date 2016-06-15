788d6bdc7030ba411a448dee8eebf3c8f36170e7
DeGsoft <diegoezequielguillen@gmail.com>
2016-06-15 15:52:47 -0300

    ios

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -50,7 +50,7 @@
         var cd = undefined;
         var time_running = undefined;
         var c = 1.6;//calibrate
-        var myMedia = new Media("sounds/bellbox.mp3")
+        var myMedia = new Media("sounds/bellbox.mp3");
         document.getElementById("btn-start").onclick = function () {
             ((this).innerText == 'start') ? play() : stop();
         };
